aff4a11997d7a9b7293ccc494cfd2b925d77c4cc
Pei-Ming Wu <p408865@gmail.com>
2020-08-02 10:23:13 +0800

    feat: add colors to profile

--- components/profile.js
+++ components/profile.js
@@ -5,10 +5,10 @@ export default function Profile() {
     const aka = '(aka mkfsn)'
     const address = 'Taiwan. p408865(#)gmail.com';
     const links = [
-        {logo: 'home', href: 'https://mkfsn.github.io'},
-        {logo: 'logo-github', href: 'https://github.com/mkfsn'},
-        {logo: 'logo-twitter', href: 'https://twitter.com/mkfsn'},
-        {logo: 'logo-linkedin', href: 'https://linkedin.com/in/mkfsn'},
+        {color: 'rgb(78,165,133)', logo: 'home', href: 'https://mkfsn.github.io'},
+        {color: 'rgb(33,37,40)', logo: 'logo-github', href: 'https://github.com/mkfsn'},
+        {color: 'rgb(43,152,236)', logo: 'logo-twitter', href: 'https://twitter.com/mkfsn'},
+        {color: 'rgb(23,104,166)', logo: 'logo-linkedin', href: 'https://linkedin.com/in/mkfsn'},
     ];
     return (
         <header id="me">
@@ -16,13 +16,13 @@ export default function Profile() {
                 <h1 className={style.name}>
                     <span>{name}</span>
                     &nbsp;
-                    <span>{aka}</span>
+                    <span className={style.aka}>{aka}</span>
                 </h1>
                 <h4 className={style.address}>{address}</h4>
                 <h4 className={style.sns}>
                     {links.map((link) => (
                         <a className={style.link} target="_blank" href={link.href} key={link.logo}>
-                            <ion-icon name={link.logo} />
+                            <ion-icon name={link.logo} style={{color: link.color}}/>
                         </a>
                     ))}
                 </h4>
