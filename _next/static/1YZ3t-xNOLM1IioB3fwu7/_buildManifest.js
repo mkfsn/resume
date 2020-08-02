a7edaad01622f0250504613d162e76453a4da44a
mkfsn <mkfsn@users.noreply.github.com>
2020-08-02 09:00:02 +0000

    feat: add favicon url - might need them in the future 6a0bbfaf2e74ca6d4ddba12340b1a294b766218b

---
+++ _next/static/1YZ3t-xNOLM1IioB3fwu7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002F4cf22e23bdc21cec4a16.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/9PqQTq9EaRYGbc8yKJIjN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002F02b526cab4e106176abd.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
