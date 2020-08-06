9d6595bccc18fc004cf63a1a959901092d57a3f3
mkfsn <mkfsn@users.noreply.github.com>
2020-08-06 11:17:54 +0000

    fix: prevent displaying black background on print when using box-shadow e388d83e899c279b8e4b4896745c51d68bbfe9a8

--- _next/static/SM5v50pYzuc5LmYF0rqHk/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002F82c52c605d72a8b05b9e.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/VnyIT5JBR9ijvhSiYgkKb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fcss\u002Ff82b7f03e55c5c0cdd81.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
