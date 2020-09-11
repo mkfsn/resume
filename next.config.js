889b3d56e45a1063e1a31a94567ba2287348db38
Pei-Ming Wu <p408865@gmail.com>
2020-09-12 01:06:21 +0800

    feat: add js-yaml-loader

--- next.config.js
+++ next.config.js
@@ -10,4 +10,13 @@ module.exports = {
     publicRuntimeConfig: {
         localeSubpaths,
     },
+    webpack: function (config) {
+        config.module.rules.push(
+            {
+                test: /\.ya?ml$/,
+                use: 'js-yaml-loader',
+            },
+        )
+        return config
+    }
 }
