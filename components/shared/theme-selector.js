7f9c4e7e30ab29273d646a2dd6fb0aad396461e5
Pei-Ming Wu <p408865@gmail.com>
2020-09-12 23:58:01 +0800

    fix: add assetPrefix for links

--- components/shared/theme-selector.js
+++ components/shared/theme-selector.js
@@ -3,6 +3,8 @@ import style from "./theme-selector.module.scss"
 import Icon from "./icon"
 
 export default function ThemeSelector() {
+    const isProd = process.env.NODE_ENV === "production"
+    const assetPrefix = isProd ? '/resume' : ''
     return (
         <div className={style.dropdown}>
             <button className={style["dropdown-btn"]}>
@@ -10,8 +12,8 @@ export default function ThemeSelector() {
                 <Icon name="arrow-dropdown-circle"/>
             </button>
             <div className={style["dropdown-content"]}>
-                <a href="/?theme=printable">Printable</a>
-                <a href="/">Prettier</a>
+                <a href={assetPrefix + "/?theme=printable"}>Printable</a>
+                <a href={assetPrefix + "/"}>Prettier</a>
             </div>
         </div>
     )
