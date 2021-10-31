650ff5bcb041321d1e029bfab13acd0715edbb86
Pei-Ming Wu <p408865@gmail.com>
2021-10-31 16:31:11 +0800

    fix: add missing utils

---
+++ src/utils/formatter.ts
@@ -0,0 +1,31 @@
+function dateFormatter(input) {
+    let date = newDate(input);
+
+    const now = new Date();
+    if (!isValidDate(date)) {
+        date = now;
+    }
+    if (now.getFullYear() === date.getFullYear() && now.getMonth() === date.getMonth()) {
+        return 'Current';
+    }
+    const monthNames = [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+    ];
+    const month = monthNames[date.getMonth()];
+    const year = date.getFullYear().toString().substr(2);
+    return `${month}'${year}`;
+}
+
+function newDate(d): Date {
+    if (d instanceof Date) {
+        return d
+    }
+    return new Date(d)
+}
+
+function isValidDate(d: Date) {
+    return (d instanceof Date) && d.getTime && !isNaN(d.getTime());
+}
+
+export {dateFormatter}
